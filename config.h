6d5ebc0eb2413cc240c963c629d7bb724e944be2
Jess <jess@xeraxera.me>
2024-03-17 18:44:14 +1300

    add bluetooth control command

--- config.h
+++ config.h
@@ -63,6 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *sscmd[]  = { "flameshot", "gui", NULL };
+static const char *bt_concmd[]  = { "bt-con", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -102,6 +103,7 @@ static const Key keys[] = {
 	{ 0,             				XF86XK_AudioRaiseVolume,   change_vol,     {.i = +5 } },
 	{ 0,             				XF86XK_AudioLowerVolume,   change_vol,     {.i = -5 } },
 	{ 0,             				XF86XK_AudioMute,	   	   toggle_mute,    {0} },
+	{ MODKEY,                       XK_l,      spawn,          {.v = bt_concmd }},
 	{ MODKEY,             			XK_s,	   spawn,    	   {.v = sscmd } },
 };
 
