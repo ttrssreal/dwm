c7f44d913d249bfb3dcd6ba1a6c51d052806c6aa
Jess <jess@jessie.cafe>
2024-12-08 01:19:57 +1300

    config: update bluetooth command

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *sscmd[]  = { "flameshot", "gui", NULL };
-static const char *bt_concmd[]  = { "bt-con", NULL };
+static const char *bt_concmd[]  = { "bluetooth-connect", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
